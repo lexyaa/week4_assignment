08726b49fff27b05b60a12fef5e52af93515b127
lexyaa <55127454+lexyaa@users.noreply.github.com>
2020-10-29 22:19:07 +0900

    assignment01(modified)

--- dut.v
+++ dut.v
@@ -12,13 +12,13 @@ input           rst_n     ;
 reg    [31:0]   cnt       ;
 reg             clk_gen   ;
 always @(posedge clk or negedge rst_n) begin
+        clk_gen  <= ~clk_gen;
         if(rst_n == 1'b0) begin
                  cnt      <= 32'd0;
                  clk_gen  <= 1'd0;
         end else begin
                  if(cnt >= num/2-1) begin
-                           cnt      <= 32'd0;
-                           clk_gen  <= ~clk_gen;
+                           cnt      <= 32'd0;                           
                  end else begin
                            cnt <= cnt + 1'b1;
                  end
